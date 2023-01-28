29d34071f0018e8a5cd30b76647e669169274520
Erlend Syljuåsen <esylju@gmail.com>
2023-01-28 11:26:17 +0100

    fix private/public variables

--- src/particle.h
+++ src/particle.h
@@ -3,7 +3,7 @@
 #include "utils.h"
 
 class Particle {
-    public:
+    private:
         // position
         vec pos;
 
@@ -19,6 +19,7 @@ class Particle {
         // collision count
         int count;
 
+    public:
         // constructor
         Particle(double x, double y, double vx, double vy, double r, double m);
 
@@ -28,17 +29,20 @@ class Particle {
         // collision time and resolve collision functions
         // particle with particle collision
         double get_collision_time_with(Particle* p);
-        void resolve_collision_with(Particle* p);
+        void resolve_collision_with(Particle* p, double epsilon);
     
         // particle with horizontal wall collision
         double get_collision_time_with_hw();
-        void resolve_collision_with_hw();
+        void resolve_collision_with_hw(double epsilon);
 
         // particle with vertical wall collision
         double get_collision_time_with_vw();
-        void resolve_collision_with_vw();
+        void resolve_collision_with_vw(double epsilon);
 
         // helper functions
         double dist_squared_to(Particle* p);
+    
+        // let << and class Gas access private variables
         friend std::ostream& operator<<(std::ostream& os, Particle* p);
+        friend class Gas;
 };
